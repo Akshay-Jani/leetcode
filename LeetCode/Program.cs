f24e671369fc098f17c86003d06ee9727eb2a9bf
Akshay <akshayjani1999@gmail.com>
2022-12-09 23:10:59 +0530

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeetCode.Solutions;
+using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Integer is: {RomanToInteger.RomanToInt("III")}");
+        }
+    }
+}
---
+++ LeetCode/Solutions/RomanToInteger.cs
@@ -0,0 +1,94 @@
+﻿namespace LeetCode.Solutions
+{
+    /// <summary>
+    /// Class created to solve below Leetcode issue:
+    /// https://leetcode.com/problems/roman-to-integer/
+    /// </summary>
+    public static class RomanToInteger
+    {
+        public static int RomanToInt(string s, int sum = 0, int current = 0)
+        {
+            if (!string.IsNullOrEmpty(s))
+            {
+                if (current >= s.Length)
+                    return sum;
+
+                bool skipTwo = false;
+
+                switch (s[current].ToString())
+                {
+                    case "I":
+                        if ((current + 1) < s.Length && s[current + 1].ToString() == "V")
+                        {
+                            skipTwo = true;
+                            sum += 4;
+                        }
+                        else if ((current + 1) < s.Length && s[current + 1].ToString() == "X")
+                        {
+                            skipTwo = true;
+                            sum += 9;
+                        }
+                        else
+                        {
+                            sum += 1;
+                        }
+
+                        break;
+                    case "V":
+                        sum += 5;
+                        break;
+                    case "X":
+                        if ((current + 1) < s.Length && s[current + 1].ToString() == "L")
+                        {
+                            skipTwo = true;
+                            sum += 40;
+                        }
+                        else if ((current + 1) < s.Length && s[current + 1].ToString() == "C")
+                        {
+                            skipTwo = true;
+                            sum += 90;
+                        }
+                        else
+                        {
+                            sum += 10;
+                        }
+
+                        break;
+                    case "L":
+                        sum += 50;
+                        break;
+                    case "C":
+                        if ((current + 1) < s.Length && s[current + 1].ToString() == "D")
+                        {
+                            skipTwo = true;
+                            sum += 400;
+                        }
+                        else if ((current + 1) < s.Length && s[current + 1].ToString() == "M")
+                        {
+                            skipTwo = true;
+                            sum += 900;
+                        }
+                        else
+                        {
+                            sum += 100;
+                        }
+
+                        break;
+                    case "D":
+                        sum += 500;
+                        break;
+                    case "M":
+                        sum += 1000;
+                        break;
+                }
+
+                if (skipTwo)
+                    current += 2;
+                else
+                    current++;
+                return RomanToInt(s, sum, current);
+            }
+            return 0;
+        }
+    }
+}
