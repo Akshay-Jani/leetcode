7cf1768af1a5fd4bbf49a076a737510a51782699
Akshay <akshayjani1999@gmail.com>
2022-12-20 23:30:55 +0530

    Trailing zeroes in factorial solved.

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,6 +11,7 @@ namespace LeetCode
             Console.WriteLine($"Answer is: {HalvesAreAlikeProblem.Run("Textbook")}");
             Console.WriteLine($"Roman is: {IntegerToRoman.Run(1994)}");
             Console.WriteLine($"Length of Last Word is: {LengthOfTheLastWord.Run("Hello World   mee")}");
+            Console.WriteLine($"Number of Trailing Zeroes is: {NumberOfTrailingZeroes.Run(13)}");
         }
     }
 }
---
+++ LeetCode/Solutions/NumberOfTrailingZeroes.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+
+namespace LeetCode.Solutions
+{
+    /// <summary>
+    /// Class created to solve below Leetcode issue:
+    /// https://leetcode.com/problems/factorial-trailing-zeroes/
+    /// </summary>
+    public static class NumberOfTrailingZeroes
+    {
+        public static int Run(int n)
+        {
+            return n == 0 ? 0 : n / 5 + Run(n / 5);
+        }
+
+        private static int TrailingZeroes(int n)
+        {
+            if (n > 0)
+            {
+                int factorialValue = GetFactorial(n);
+
+                if (factorialValue > 0)
+                {
+                    //return CheckIfDividedByTen(factorialValue, 0);
+                    return factorialValue.ToString().Count(x => x == '0');
+                }
+            }
+            return 0;
+        }
+
+        private static int GetFactorial(int n)
+        {
+            return n == 1 ? n : n * GetFactorial(n - 1);
+        }
+
+        private static int CheckIfDividedByTen(int n, int count)
+        {
+            return n % 10 == 0 ? CheckIfDividedByTen(n / 10, count + 1) : count;
+        }
+    }
+}
